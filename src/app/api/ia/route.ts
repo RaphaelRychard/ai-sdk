31a1ded3c0a3e505a98e6dc9314c7a38ea805338
raphaelrychard <raph.rych@gmail.com>
2025-07-27 23:22:38 -0400

    add

--- src/app/api/ia/route.ts
+++ src/app/api/ia/route.ts
@@ -1,15 +1,20 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-import { generateText } from 'ai';
+import { generateObject } from 'ai';
 import { openrouter } from "@/ia/open-router";
+import { z } from "zod";
 
 export async function GET(request: NextRequest) {
-  const result = await generateText({
-    model: openrouter.chat('openai/gpt-4o-mini'),
-    prompt: 'Traduza "Hello Word" para português!',
+  const result = await generateObject({
+    model: openrouter.chat('openai/gpt-4o-2024-11-20'),
+    schema: z.object({
+      pt: z.string().describe('Tradução para português'),
+      fr: z.string().describe('Tradução para francês'),
+      es: z.string().describe('Tradução para espanhol')
+    }),
+    prompt: 'Traduza "Hello Word" para diferentes idiomas!',
     system: 'Você é uma IA especializa em tradução, sempre retorne de maneira mais sucinta possível.',
   })
 
-  return NextResponse.json({ message: result.text })
+  return NextResponse.json({ message: result.object })
 }
-
